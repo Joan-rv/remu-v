defed5ab9f13299f8a86eb7927b10919d13cf566
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-07-13 16:48:00 +0200

    Add clang format

--- samples/stdlib.c
+++ samples/stdlib.c
@@ -1,23 +1,23 @@
 #include <stdlib.h>
 void exit(int code) {
-  register int a0 asm("a0") = code;
-  asm("li a7, 93;"
-      "ecall"
-      :
-      : "r"(a0)
-      : "a7");
-  for (;;) // in case syscall fails
-    ;
+    register int a0 asm("a0") = code;
+    asm("li a7, 93;"
+        "ecall"
+        :
+        : "r"(a0)
+        : "a7");
+    for (;;) // in case syscall fails
+        ;
 }
 
 ssize_t write(int fd, const void *buf, size_t count) {
-  register int a0 asm("a0") = fd;
-  register const void *a1 asm("a1") = buf;
-  register size_t a2 asm("a2") = count;
-  asm("li a7, 64;"
-      "ecall;"
-      : "+r"(a0)
-      : "r"(a0), "r"(a1), "r"(a2)
-      : "a7");
-  return a0;
+    register int a0 asm("a0") = fd;
+    register const void *a1 asm("a1") = buf;
+    register size_t a2 asm("a2") = count;
+    asm("li a7, 64;"
+        "ecall;"
+        : "+r"(a0)
+        : "r"(a0), "r"(a1), "r"(a2)
+        : "a7");
+    return a0;
 }
--- samples/test.c
+++ samples/test.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 int main(void) {
-  char str[] = "Hello\n";
-  ssize_t n = write(1, str, sizeof(str));
-  if (n != sizeof(str))
-    return -1;
-  return 0;
+    char str[] = "Hello\n";
+    ssize_t n = write(1, str, sizeof(str));
+    if (n != sizeof(str))
+        return -1;
+    return 0;
 }
