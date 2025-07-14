9cc79355beffd26d17e0ab9db04fabbf847917ae
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-07-14 09:47:10 +0200

    Add header guards

--- samples/stdlib.h
+++ samples/stdlib.h
@@ -1,5 +1,10 @@
+#ifndef __RV32_STDLIB_H
+#define __RV32_STDLIB_H
+
 typedef unsigned int size_t; // 32-bit risc-v
 typedef int ssize_t;
 
 [[noreturn]] void exit(int code);
 int write(int fd, const void *buf, size_t count);
+
+#endif
